155d11d9ed84653c329da591e378dde26094e9a8
itsRunic <87670203+itsRunic@users.noreply.github.com>
2023-02-17 17:14:13 -0600

    Add Button handlers

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,48 @@
 import './Header.scss';
 import { BsFillCaretRightFill } from 'react-icons/bs';
+import { useEffect, useState } from 'react';
 
 interface Props {}
 
 const Header = ({}) => {
+  const [isDayShift, setIsDayShift] = useState<boolean>(true);
+  const [isClearviewHouse, setIsClearviewHouse] = useState<boolean>(true);
+
+  // House Buttons
+  const houseButtonHandler = () => {
+    setIsClearviewHouse(!isClearviewHouse);
+  };
+
+  const changeCVHouseButtonColor = (selectedHouse: boolean) => {
+    return isClearviewHouse ? 'house-button selected-house' : 'house-button';
+  };
+
+  const changeWLHouseButtonColor = (selectedHouse: boolean) => {
+    return !isClearviewHouse ? 'house-button selected-house' : 'house-button';
+  };
+
+  const changeArrowDirection = (selectedHouse: boolean) => {
+    return isClearviewHouse ? 'icon left-icon' : 'icon';
+  };
+
+  // Shift Buttons
+  const shiftButtonHandler = () => {
+    setIsDayShift(!isDayShift);
+  };
+
+  const changeDayShiftButtonColor = (selectedShift: boolean) => {
+    return isDayShift ? 'shift-button selected-shift' : 'shift-button';
+  };
+
+  const changeNightShiftButtonColor = (selectedShift: boolean) => {
+    return !isDayShift ? 'shift-button selected-shift' : 'shift-button';
+  };
+
+  useEffect(() => {
+    console.log('House:', isClearviewHouse);
+    console.log('day:', isDayShift);
+  }, [isClearviewHouse, isDayShift]);
+
   return (
     <header>
       <div className="primary-heading-container">
@@ -12,20 +51,38 @@ const Header = ({}) => {
       </div>
 
       <div className="house-button-wrapper">
-        <button>
+        <button
+          onClick={houseButtonHandler}
+          className={changeCVHouseButtonColor(isClearviewHouse)}
+        >
           <span>Clearview</span>
         </button>
-        <button>
-          <BsFillCaretRightFill className="icon" />
+        <button onClick={houseButtonHandler} className="house-button">
+          <BsFillCaretRightFill
+            className={changeArrowDirection(isClearviewHouse)}
+          />
         </button>
-        <button>
+        <button
+          onClick={houseButtonHandler}
+          className={changeWLHouseButtonColor(isClearviewHouse)}
+        >
           <span>Williston</span>
         </button>
       </div>
 
       <div className="shift-button-wrapper">
-        <button>Day Shift</button>
-        <button>Night Shift</button>
+        <button
+          onClick={shiftButtonHandler}
+          className={changeDayShiftButtonColor(isDayShift)}
+        >
+          Day Shift
+        </button>
+        <button
+          onClick={shiftButtonHandler}
+          className={changeNightShiftButtonColor(isDayShift)}
+        >
+          Night Shift
+        </button>
       </div>
     </header>
   );
