c13fbb5ad435fd9bba9bdba4b63abc0acc304105
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-03-01 18:53:52 -0600

    Add ScheduleForm component

---
+++ client/src/components/ScheduleForm/ScheduleForm.tsx
@@ -0,0 +1,14 @@
+import Modal from '../UI/Modal/Modal';
+import './ScheduleForm.scss';
+
+interface Props {}
+
+const ScheduleForm = ({}) => {
+  return (
+    <Modal>
+      <form></form>
+    </Modal>
+  );
+};
+
+export default ScheduleForm;
