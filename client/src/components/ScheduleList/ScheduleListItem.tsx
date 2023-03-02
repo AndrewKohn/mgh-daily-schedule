0ba91cd2cc9a66b601d5e2e7f60a10d5e098bdb7
Andrew Kohn <contact@kohnandrew.com>
2023-03-02 07:15:05 -0600

    add icons to list items

--- client/src/components/ScheduleList/ScheduleListItem.tsx
+++ client/src/components/ScheduleList/ScheduleListItem.tsx
@@ -1,26 +1,60 @@
+import { useEffect, useState } from 'react';
+import { BsFillExclamationCircleFill, BsPencilSquare } from 'react-icons/bs';
 import './ScheduleListItem.scss';
+import ScheduleItem from '../../store/ScheduleListModel';
 
 interface Props {}
 
 const ScheduleListItem = ({}) => {
+  const [scheduleItem, setScheduleItem] = useState<ScheduleItem>();
+
+  // useEffect(() => {
+  //   setScheduleItem({
+  //     id: 11,
+  //     patientName: 'Dane Joe',
+  //     activityTime: 9,
+  //     activityTitle: 'Medication Administration',
+  //     activityNote:
+  //       'Insulin instructions...  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut consequuntur odit nemo repudiandae. Deserunt numquam nulla qui error ipsum, quas maiores quasi asperiores odio labore aliquid unde facere at tenetur.',
+  //     isImportant: true,
+  //     isComplete: false,
+  //     isEdit: true,
+  //   });
+  // }, []);
+
   return (
     <li className="schedule-list-item">
       <span className="schedule-list-item-information">
-        <p data-testid="required-time">10AM</p>
-        <p data-testid="required-name">Doe, Jane</p>
+        <p data-testid="required-time">{scheduleItem?.activityTime}AM</p>
+        <p data-testid="required-name">{scheduleItem?.patientName}</p>
         <details className="activity-container">
           <summary data-testid="required-activity" className="activity-title">
-            Activity Title
+            {scheduleItem?.isImportant ? (
+              <span className="important">
+                <BsFillExclamationCircleFill />
+              </span>
+            ) : (
+              ''
+            )}
+
+            {scheduleItem?.activityTitle}
           </summary>
-          <p className="activity-details">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut
-            consequuntur odit nemo repudiandae. Deserunt numquam nulla qui error
-            ipsum, quas maiores quasi asperiores odio labore aliquid unde facere
-            at tenetur.
-          </p>
+          <p className="activity-details">{scheduleItem?.activityNote}</p>
         </details>
-        <span className="checkbox-container">
-          <input className="checkbox" type="checkbox" />
+        <span className="action-container">
+          {scheduleItem?.isEdit ? (
+            <button className="edit-button">
+              <div className="edit-icon">
+                <BsPencilSquare />
+              </div>
+            </button>
+          ) : (
+            <input
+              className="checkbox"
+              type="checkbox"
+              defaultChecked={scheduleItem?.isComplete}
+            />
+          )}
         </span>
       </span>
     </li>
