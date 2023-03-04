395904444cf09b674787a835f1d3d32a15fb7ee3
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-03-04 02:29:36 -0600

    Add edit button for ScheduleForm component

--- client/src/components/ScheduleList/ScheduleList.tsx
+++ client/src/components/ScheduleList/ScheduleList.tsx
@@ -1,19 +1,40 @@
+import { Fragment, useContext, useState } from 'react';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
+import AdminContext from '../../store/AdminContext';
+import ScheduleForm from '../ScheduleForm/ScheduleForm';
 import './ScheduleList.scss';
 import ScheduleListItem from './ScheduleListItem';
 
 interface Props {}
 
 const ScheduleList = ({}) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const adminContext = useContext(AdminContext);
+
+  const showFormHandler = (e: any) => {
+    setIsVisible(true);
+  };
+
   return (
-    <ul className="schedule-list">
-      <span className="list-headings">
-        <p>Time</p>
-        <p>Patient</p>
-        <p>Activity</p>
-        <p>&nbsp;</p>
-      </span>
-      <ScheduleListItem />
-    </ul>
+    <Fragment>
+      <ul className="schedule-list">
+        <span className="list-headings">
+          <p>Time</p>
+          <p>Patient</p>
+          <p>Activity</p>
+          <p>&nbsp;</p>
+        </span>
+        <ScheduleListItem />
+      </ul>
+      {adminContext.isLoggedIn && (
+        <button className="add-icon-container" onClick={showFormHandler}>
+          <BsFillPlusCircleFill className="add-icon" />
+        </button>
+      )}
+      {isVisible && (
+        <ScheduleForm isVisible={isVisible} setIsVisible={setIsVisible} />
+      )}
+    </Fragment>
   );
 };
 
