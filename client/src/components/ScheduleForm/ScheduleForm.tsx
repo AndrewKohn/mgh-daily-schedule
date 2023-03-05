03d8114292c2af1ee1aa92ff250563a38ec4ea6c
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-03-05 03:27:05 -0600

    Update to a cleaner form layout

--- client/src/components/ScheduleForm/ScheduleForm.tsx
+++ client/src/components/ScheduleForm/ScheduleForm.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { FcCheckmark } from 'react-icons/fc';
+import { IoMdRemoveCircleOutline } from 'react-icons/io';
 import Modal from '../UI/Modal/Modal';
 import './ScheduleForm.scss';
 
@@ -37,6 +39,7 @@ const ScheduleForm = ({ isVisible, setIsVisible }: Props) => {
     '10PM',
     '11PM',
   ];
+
   const patientNames = [
     'Adams, Dorothy',
     'Butter, Peanut',
@@ -62,36 +65,81 @@ const ScheduleForm = ({ isVisible, setIsVisible }: Props) => {
 
   return (
     <Modal isVisible={isVisible} setIsVisible={setIsVisible}>
-      <form>
-        <h2>New Activity</h2>
-        <label htmlFor="time">Time</label>
-        <select
-          id="time"
-          name="time"
-          value={time}
-          onChange={setTimeHandler}
-          autoFocus
-        >
-          {optionTimes.map((selectedTime: string, key: number) => (
-            <option key={key}>{selectedTime}</option>
-          ))}
-        </select>
-        <label htmlFor="patient">Patient</label>
-        <select
-          id="patient"
-          name="patient"
-          value={patient}
-          onChange={setPatientHandler}
-        >
-          {patientNames.map((name: string, key: number) => (
-            <option key={key}>{name}</option>
-          ))}
-        </select>
-        <label htmlFor="activity-title">Activity Title</label>
-        <input id="activity-title" name="activity-title" type="text" />
-        <label htmlFor="activity-notes">Activity Notes</label>
-        <textarea id="activity-notes" name="activity-notes" />
-        <button>Submit</button>
+      <form className="schedule-form">
+        <h2 className="form-title">New Activity</h2>
+        <div className="form--input-container">
+          <div className="form--selection-wrapper">
+            <label className="form--label" htmlFor="time">
+              Time
+            </label>
+            <select
+              id="time"
+              name="time"
+              className="form--input-select"
+              value={time}
+              onChange={setTimeHandler}
+              autoFocus
+            >
+              {optionTimes.map((selectedTime: string, key: number) => (
+                <option key={key}>{selectedTime}</option>
+              ))}
+            </select>
+          </div>
+          <div className="form--selection-wrapper">
+            <label className="form--label" htmlFor="patient">
+              Patient
+            </label>
+            <select
+              id="patient"
+              name="patient"
+              className="form--input-select"
+              value={patient}
+              onChange={setPatientHandler}
+            >
+              {patientNames.map((name: string, key: number) => (
+                <option key={key}>{name}</option>
+              ))}
+            </select>
+            <button className="icon icon--add" type="button">
+              <FcCheckmark />
+            </button>
+            <button className="icon icon--delete" type="button">
+              <IoMdRemoveCircleOutline />
+            </button>
+          </div>
+          <div className="form--selection-wrapper">
+            <label htmlFor="activity-importance">Important</label>
+            <input
+              id="activity-importance"
+              className="important-checkbox"
+              type="checkbox"
+            />
+          </div>
+        </div>
+        <div className="form--input-wrapper">
+          <label className="form--label" htmlFor="activity-title">
+            Activity Title
+          </label>
+          <input
+            className="form--input"
+            id="activity-title"
+            name="activity-title"
+            type="text"
+          />
+        </div>
+        <div className="form--input-wrapper">
+          <label className="form--label" htmlFor="activity-notes">
+            Activity Notes
+          </label>
+          <textarea
+            className="form--input-textarea"
+            id="activity-notes"
+            name="activity-notes"
+          />
+        </div>
+        <button className="form--button" type="submit">
+          Submit
+        </button>
       </form>
     </Modal>
   );
