7f9f8fecb163e27bd56be39ee25231a429984bd7
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-03-05 03:21:42 -0600

    Add Home page component

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,56 @@
+import Card from '../components/UI/Card/Card';
+import dailyScheduleIcon from '../icons/daily-schedule-icon.png';
+import staffScheduleIcon from '../icons/staff-schedule-icon.png';
+import loginIcon from '../icons/login-icon.png';
+import './Home.scss';
+import { useEffect, useState } from 'react';
+import Login from '../components/Login/Login';
+import { Link } from 'react-router-dom';
+
+interface Props {}
+
+const Home = ({}) => {
+  const [showLogin, setShowLogin] = useState<boolean>(false);
+
+  useEffect(() => console.log(showLogin), [showLogin]);
+
+  const showLoginHandler = () => {
+    setShowLogin(false);
+    setTimeout(() => {
+      setShowLogin(true);
+    }, 10);
+  };
+
+  return (
+    <div className="login-container">
+      <Card classes="nav-card">
+        <h2 className="login-heading">Morning Glory Homes</h2>
+        <nav className="home-nav">
+          <Link to="/daily-schedule" className="link">
+            <img
+              className="icon"
+              src={dailyScheduleIcon}
+              alt="daily schedule icon"
+            />
+            <p className="link-text">Daily Schedule</p>
+          </Link>
+          <Link to="/staff-schedule" className="link">
+            <img
+              className="icon"
+              src={staffScheduleIcon}
+              alt="staff schedule icon"
+            />
+            <p className="link-text">Staff Schedule</p>
+          </Link>
+          <button className="button link" onClick={showLoginHandler}>
+            <img className="icon" src={loginIcon} alt="login icon" />
+            <p className="link-text">Admin Login</p>
+          </button>
+        </nav>
+      </Card>
+      {showLogin && <Login />}
+    </div>
+  );
+};
+
+export default Home;
