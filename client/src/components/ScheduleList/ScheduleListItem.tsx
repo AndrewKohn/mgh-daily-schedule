5a6a881338cd07554d517a81e54ba51e1fea219f
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-03-11 11:48:31 -0600

    Add scheduleItem prop & list now show correct time

--- client/src/components/ScheduleList/ScheduleListItem.tsx
+++ client/src/components/ScheduleList/ScheduleListItem.tsx
@@ -3,26 +3,30 @@ import { BsFillExclamationCircleFill, BsPencilSquare } from 'react-icons/bs';
 import './ScheduleListItem.scss';
 import ScheduleItem from '../../store/ScheduleListModel';
 import AdminContext from '../../store/AdminContext';
+import optionTimes from '../../store/OptionTimes';
 
-interface Props {}
+interface Props {
+  scheduleItem: ScheduleItem;
+}
 
-const ScheduleListItem = ({}) => {
-  const [scheduleItem, setScheduleItem] = useState<ScheduleItem>();
+const ScheduleListItem = ({ scheduleItem }: Props) => {
+  const [scheduleListItem, setScheduleListItem] =
+    useState<ScheduleItem>(scheduleItem);
   const adminContext = useContext(AdminContext);
 
-  useEffect(() => {
-    setScheduleItem({
-      id: 11,
-      patientName: 'Dane Joe',
-      activityTime: 9,
-      activityTitle: 'Medication Administration',
-      activityNote:
-        'Insulin instructions...  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut consequuntur odit nemo repudiandae. Deserunt numquam nulla qui error ipsum, quas maiores quasi asperiores odio labore aliquid unde facere at tenetur.',
-      isImportant: true,
-      isComplete: false,
-      isEdit: false,
-    });
-  }, []);
+  // useEffect(() => {
+  //   setScheduleListItem({
+  //     id: 11,
+  //     patientName: 'Dane Joe',
+  //     activityTime: 9,
+  //     activityTitle: 'Medication Administration',
+  //     activityNote:
+  //       'Insulin instructions...  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut consequuntur odit nemo repudiandae. Deserunt numquam nulla qui error ipsum, quas maiores quasi asperiores odio labore aliquid unde facere at tenetur.',
+  //     isImportant: true,
+  //     isComplete: false,
+  //     isEdit: false,
+  //   });
+  // }, []);
 
   const activityImportance = () => {
     return scheduleItem?.isImportant ? (
@@ -53,7 +57,9 @@ const ScheduleListItem = ({}) => {
   return (
     <li className="schedule-list-item">
       <span className="schedule-list-item-information">
-        <p data-testid="required-time">{scheduleItem?.activityTime}AM</p>
+        <p data-testid="required-time">
+          {optionTimes[scheduleItem?.activityTime]}
+        </p>
         <p data-testid="required-name">{scheduleItem?.patientName}</p>
         <details className="activity-container">
           <summary data-testid="required-activity" className="activity-title">
