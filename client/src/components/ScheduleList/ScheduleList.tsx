29f2002ab83f552af063980d86466fefbf6c6469
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-03-10 18:23:47 -0600

    Add form submission handler

--- client/src/components/ScheduleList/ScheduleList.tsx
+++ client/src/components/ScheduleList/ScheduleList.tsx
@@ -1,19 +1,54 @@
-import { Fragment, useContext, useState } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 import AdminContext from '../../store/AdminContext';
 import ScheduleForm from '../ScheduleForm/ScheduleForm';
 import './ScheduleList.scss';
 import ScheduleListItem from './ScheduleListItem';
+import ScheduleItem from '../../store/ScheduleListModel';
 
 interface Props {}
 
 const ScheduleList = ({}) => {
+  let idCount: number = 0;
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const adminContext = useContext(AdminContext);
+  const [scheduleItem, setScheduleItem] = useState<ScheduleItem | undefined>(
+    undefined
+  );
+
+  useEffect(() => console.log(scheduleItem), []);
 
   const showFormHandler = (e: any) => {
     setIsVisible(true);
   };
+
+  const submitFormHandler = (
+    e: any,
+    patientName: string,
+    activityTime: string,
+    activityTitle: string,
+    isImportant: boolean,
+    activityNote?: string
+  ) => {
+    e.preventDefault();
+
+    // setScheduleItem({
+    //   id: idCount++,
+    //   patientName: patientName,
+    //   activityTime: time,
+    //   activityTitle: activityTitle,
+    //   activityNote: activityNote,
+    //   isImportant: isImportant,
+    //   isComplete: false,
+    //   isEdit: false,
+    // });
+
+    // [TODO]
+    // activityTime length can be 3 or 4.
+    console.log(activityTime);
+  };
+
+  useEffect(() => console.log(scheduleItem), [scheduleItem]);
 
   return (
     <Fragment>
@@ -32,7 +67,11 @@ const ScheduleList = ({}) => {
         </button>
       )}
       {isVisible && (
-        <ScheduleForm isVisible={isVisible} setIsVisible={setIsVisible} />
+        <ScheduleForm
+          submitFormHandler={submitFormHandler}
+          isVisible={isVisible}
+          setIsVisible={setIsVisible}
+        />
       )}
     </Fragment>
   );
