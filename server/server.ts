992fe661e22d8cf3eeed206bab6f44358e2b8d9e
Andrew Kohn <contact@kohnandrew.com>
2023-03-24 02:15:28 -0500

    Update server.ts to show errors

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,25 @@
-import express, { Express, Request, Response } from 'express';
+require('dotenv').config();
+const express = require('express');
+import { Express, Request, Response, NextFunction } from 'express';
 const app: Express = express();
-const PORT = 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send("Yes you're on the right path...");
+app.use(express.json());
+
+app.use('/daily_schedule', require('./routes/DailyScheduleRoutes'));
+
+// Global error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.stack);
+  console.log(err.name);
+  console.log(err.code);
+
+  res.status(500).json({
+    message: 'Server error!  Something went wrong...',
+  });
 });
 
-app.get('/hi', (req: Request, res: Response) => {
-  res.send('Hello there!');
-});
-
+// Listen on pc port
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Now listening on Port ${PORT}`);
+  console.log(`Server is currently listening to PORT: ${PORT}`);
 });
