47e9121cde3757f4c73c60ded242355e81121308
Andrew Kohn <contact@kohnandrew.com>
2023-03-24 02:13:57 -0500

    Update test for ScheduleListItem

--- client/src/components/ScheduleList/ScheduleListItem.test.tsx
+++ client/src/components/ScheduleList/ScheduleListItem.test.tsx
@@ -4,21 +4,63 @@ import { render, screen } from '@testing-library/react';
 
 describe('<ScheduleListItem />', () => {
   it('Time element exists and is visible', () => {
-    render(<ScheduleListItem />);
+    render(
+      <ScheduleListItem
+        scheduleItem={{
+          id: 11,
+          patientName: 'Dane Joe',
+          activityTime: 9,
+          activityTitle: 'Medication Administration',
+          activityNote:
+            'Insulin instructions...  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut consequuntur odit nemo repudiandae. Deserunt numquam nulla qui error ipsum, quas maiores quasi asperiores odio labore aliquid unde facere at tenetur.',
+          isImportant: false,
+          isComplete: false,
+          isEdit: false,
+        }}
+      />
+    );
 
     const requiredTime = screen.getByTestId('required-time');
     expect(requiredTime).toBeInTheDocument();
   });
 
   it('Patient Name element exists and is visible', () => {
-    render(<ScheduleListItem />);
+    render(
+      <ScheduleListItem
+        scheduleItem={{
+          id: 11,
+          patientName: 'Dane Joe',
+          activityTime: 9,
+          activityTitle: 'Medication Administration',
+          activityNote:
+            'Insulin instructions...  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut consequuntur odit nemo repudiandae. Deserunt numquam nulla qui error ipsum, quas maiores quasi asperiores odio labore aliquid unde facere at tenetur.',
+          isImportant: false,
+          isComplete: false,
+          isEdit: false,
+        }}
+      />
+    );
 
     const requiredName = screen.getByTestId('required-name');
     expect(requiredName).toBeInTheDocument();
   });
 
   it('Activity element exists and is visible', () => {
-    render(<ScheduleListItem />);
+    render(
+      <ScheduleListItem
+        scheduleItem={{
+          id: 11,
+          patientName: 'Dane Joe',
+          activityTime: 9,
+          activityTitle: 'Medication Administration',
+          activityNote:
+            'Insulin instructions...  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut consequuntur odit nemo repudiandae. Deserunt numquam nulla qui error ipsum, quas maiores quasi asperiores odio labore aliquid unde facere at tenetur.',
+          isImportant: false,
+          isComplete: false,
+          isEdit: false,
+        }}
+      />
+    );
 
     const requiredActivity = screen.getByTestId('required-activity');
     expect(requiredActivity).toBeInTheDocument();
