45f32520648f17f33393c8da37d5d339d85106e5
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-06-27 23:35:10 -0500

    Add db get functions

--- client/src/pages/DailySchedule.tsx
+++ client/src/pages/DailySchedule.tsx
@@ -1,12 +1,67 @@
 import axios from 'axios';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import Header from '../components/Header/Header';
 import ScheduleList from '../components/ScheduleList/ScheduleList';
 import ScheduleItem from '../store/ScheduleListModel';
+import StaffShiftContext from '../store/StaffShiftContext';
+import Patient from '../store/PatientModel';
+
+interface DBConfig {
+  path: string;
+  target: string;
+}
 
 const DailySchedule = ({}) => {
+  const staffShiftContext = useContext(StaffShiftContext);
   const [dbData, setDBData] = useState<any>([]);
   const [dbScheduleItems, setDBScheduleItems] = useState<ScheduleItem[]>([]);
+  const [clearviewDB, setClearviewDB] = useState<ScheduleItem[]>([]);
+  const [willistonDB, setWillistonDB] = useState<ScheduleItem[]>([]);
+  const [patients, setPatients] = useState<Patient[]>([]);
+
+  const clearviewConfig: DBConfig = {
+    path: '/clearview',
+    target: 'clearviewDailySchedule',
+  };
+
+  const willistonConfig: DBConfig = {
+    path: '/williston',
+    target: 'willistonDailySchedule',
+  };
+
+  const patientsConfig: DBConfig = {
+    path: '/patients',
+    target: 'patients',
+  };
+
+  useEffect(() => {
+    // [TODO] : Maybe this might need setInterval to render update information
+    getDBData(patientsConfig, patients, setPatients);
+
+    staffShiftContext.isClearViewHouse
+      ? getDBData(clearviewConfig, clearviewDB, setClearviewDB)
+      : getDBData(willistonConfig, willistonDB, setWillistonDB);
+  }, [staffShiftContext.isClearViewHouse, patients]);
+
+  useEffect(() => {
+    console.log('patients:', patients);
+    console.log('cv:', clearviewDB);
+    console.log('wl:', willistonDB);
+  }, [patients, clearviewDB, willistonDB]);
+
+  const getDBData = (
+    config: DBConfig,
+    state: any,
+    setState: React.Dispatch<React.SetStateAction<any>>
+  ) => {
+    const { path, target } = config;
+    axios.get('http://localhost:3000' + path).then(res => {
+      if (JSON.stringify(state) !== JSON.stringify(res.data[target]))
+        setState(res.data[target]);
+    });
+  };
+
+  ///////////////////////////////////////////////////////////////
 
   useEffect(() => {
     // Initial GET
@@ -46,6 +101,8 @@ const DailySchedule = ({}) => {
       setDBScheduleItems([...newDbScheduleItems]);
     }
   }, [dbData]);
+
+  //////////////////////////////////////////////////////
 
   return (
     <Fragment>
