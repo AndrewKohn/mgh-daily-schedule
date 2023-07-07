7f2dfc11126ff7a8852dadd8d0e2ba4e0d1d4b11
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-07-07 04:50:12 -0500

    Add logs for server info

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,30 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const startTime = Date.now();
+  const currentDate = new Date();
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    timeZone: 'America/Chicago',
+    hour12: false,
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
+  const dateFormat = `${currentDate.getMonth()}/${currentDate.getDay()}/${currentDate.getFullYear()}`;
+  const centralTime = currentDate.toLocaleString('en-US', timeOptions);
+  const urlPath: string = req.baseUrl + req.originalUrl;
+
+  res.on('finish', () => {
+    const responseTime = Date.now() - startTime;
+    console.log(
+      `${dateFormat} ${centralTime}: ${responseTime}ms | [${req.method}] (${res.statusCode}) ${urlPath}`
+    );
+  });
+
+  next();
+});
+app.use('/daily_schedule', require('./routes/DailyScheduleRoutes'));
 app.use('/clearview', require('./routes/ClearviewScheduleRoutes'));
 app.use('/williston', require('./routes/WillistonScheduleRoutes'));
 app.use('/patients', require('./routes/PatientsRoutes'));
